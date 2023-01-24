3d5e9c496284a41382c9732028cc71ba7cf244f5
may <mtruong@hotmail.co.uk>
2023-01-24 20:37:42 +0000

    updated code and added js files

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "Which exercise targets the hamstrings?",
+        "choices": ["Lat pulldown", "Sumo deadlift", "Plank", "Jumping jacks"],
+        "answer": "Sumo deadlift"
+    },
+    {
+        "title": "How many parts/bones is the shoulder made up of?",
+        "choices": ["1", "2", "3", "4"],
+        "answer": "3"   
+    },
+    {
+        "title": "What does the exericse, overhead press, target?",
+        "choices": ["Triceps", "Biceps", "Shoulders", "Abs"],
+        "answer": "Triceps"   
+    }
+]
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,9 +1,18 @@
 function printScoreboard() {
+    let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+    highScores.sort(function(a, b) {
+        return b.score - a.score;
+    })
+
+    highScores.forEach(function(score) {
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(list);
+    })
 }
 
-function clearScoreboard() {
 
-}
-
-let clearButton = document.getElementById("clear");
+printScoreboard();
